43cd7eeeeb9ccbe9b13356d4fe471e4457012c5d
Frank Laub <frank.laub@gmail.com>
2015-04-04 15:35:06 -0700

    Generalize solution to work with different data files with slightly different requirements

--- src/kata4.rs
+++ src/kata4.rs
@@ -1,7 +1,6 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::iter::Filter;
-use std::str::FromStr;
 use std::str::Split;
 
 struct Words<'a> {
@@ -25,46 +24,93 @@ fn words(s: &str) -> Words {
 }
 
 fn parse_temperature(s: &str) -> i32 {
-	let digits = s.split('*').collect::<Vec<&str>>();
-	i32::from_str(digits[0]).unwrap()
+	s.trim_right_matches("*").parse::<i32>().unwrap()
 }
 
-pub fn parse_weather(line_begin: usize, line_end: usize) -> (i32, i32) {
-	let mut min_delta = i32::max_value();
-	let mut min_day = 0;
+struct Data<T> {
+	key: T,
+	value1: i32,
+	value2: i32,
+}
 
-	let fin = File::open("data/weather.dat").unwrap();
+struct Accumulator<T> {
+	key: T,
+	value: i32,
+}
+
+fn min_delta<K>(acc: Accumulator<K>, item: Data<K>) -> Accumulator<K> {
+	let delta = (item.value1 - item.value2).abs();
+	if delta < acc.value { 
+		Accumulator { key: item.key, value: delta }
+	}
+	else { 
+		acc
+	}
+}
+
+fn parse_file<K, F>(filename: &str, default: K, pluck: F) -> (K, i32) 
+	where F : Fn(Vec<&str>) -> Option<Data<K>> {
+
+	let fin = File::open(filename).unwrap();
 	let reader = BufReader::new(fin);
 	let lines = reader.lines();
 
-	for (i, item) in lines.enumerate() {
-		let line = item.unwrap();
+	let init = Accumulator { key: default, value: i32::max_value() };
 
-		if i >= line_begin && i <= line_end {
-			let columns = words(&line).collect::<Vec<&str>>();
-			let day = i32::from_str(columns[0]).unwrap();
-			let max = parse_temperature(columns[1]);
-			let min = parse_temperature(columns[2]);
-			// println!("{}: ({}, {})", day, max, min);
-			
-			let delta = max - min;
-			if delta < min_delta {
-				min_day = day;
-				min_delta = delta;
-			}
+	let result = lines.filter_map(|x| {
+		let line = x.unwrap();
+		let columns = words(&line).collect::<Vec<&str>>();
+		pluck(columns)
+	}).fold(init, min_delta);
+
+	return (result.key, result.value);
+}
+
+pub fn parse_weather() -> (i32, i32) {
+	parse_file("data/weather.dat", 0, |columns| {
+		if columns.len() < 3 {
+			return None;
 		}
-	}
-	
-	// println!("min delta({}): day {}", min_delta, min_day);
-	return (min_day, min_delta);
+
+		let day = match columns[0].parse::<i32>() {
+			Ok(n) => n,
+			Err(_) => return None
+		};
+
+		return Some(Data {
+			key: day,
+			value1: parse_temperature(columns[1]),
+			value2: parse_temperature(columns[2]),
+		});
+	})
+}
+
+pub fn parse_football() -> (String, i32) {
+	parse_file("data/football.dat", String::new(), |columns| {
+		if columns.len() != 10 {
+			return None;
+		}
+
+		return Some(Data {
+			key: columns[1].to_string(),
+			value1: columns[6].parse::<i32>().unwrap(),
+			value2: columns[8].parse::<i32>().unwrap(),
+		});
+	})
 }
 
 #[cfg(test)]
 mod test {
 	use super::parse_weather;
+	use super::parse_football;
 
 	#[test]
 	fn test_weather() {
-		assert!((14, 2) == parse_weather(8, 37))
+		assert!((14, 2) == parse_weather())
+	}
+
+	#[test]
+	fn test_football() {
+		assert!(("Aston_Villa".to_string(), 1) == parse_football())
 	}
 }
